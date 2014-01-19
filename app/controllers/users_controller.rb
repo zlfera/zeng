68570a82b76df9b95503686bb8e4110d5ab6cef7
曾令发 <zlfera@gmail.com>
2014-01-19 13:07:26 +0000

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def create
     @user=User.new(params[:user])
+    if @user.save
+      p 1
+    else
+      render 'new'
+    end
+  end
   
   def new
     @user=User.new    
