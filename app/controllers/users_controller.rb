f7bcf865fad4908e290662d537dff8212abfbd74
曾令发 <zlfera@gmail.com>
2014-01-23 23:36:20 +0000

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,9 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
 end
