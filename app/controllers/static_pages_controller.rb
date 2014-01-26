60c399fc6aeabfa0fb6cb2358a868e24debfe308
曾令发 <zlfera@gmail.com>
2014-01-26 13:25:04 +0000

    0

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,4 @@ class StaticPagesController < ApplicationController
 
 
 
-end
+end 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -14,9 +14,20 @@ describe "UsersPages" do
   describe "sigup page" do
     before {visit signup_path}
     let(:submit) {'create my account'}
+#    describe 'with invalid information' do
+#      it 'should not create user' do
+#        expect {click_button submit}.not_to change(User,:count)
+#      end
+#    end
     describe 'with valid information' do
-      it 'should not create user' do
-        expect {click_button submit}.not_to change(User,:count)
+      before do
+        fill_in('Name',with: 'example user')
+        fill_in('Email',with: 'example@user.com')
+        fill_in('Password',with: 'password')
+        fill_in('Confirmation',with: 'password')
+      end
+      it "should create user" do
+        expect {click_button(submit).to(change(User,:count).by(10))}
       end
     end
     
