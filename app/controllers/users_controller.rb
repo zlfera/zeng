c41b5e75fa55091686e540e138cc0d5546bcde2a
曾令发 <zlfera@gmail.com>
2014-01-26 13:42:27 +0000

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def create
-    @user=User.new(params[:user])
+    @user=User.new(user_params)
     if @user.save
       redirect_to(@user)
     else
