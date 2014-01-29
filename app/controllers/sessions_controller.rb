e71a94461283e2700f6140c3d5f931c2c6b69377
曾令发 <zlfera@gmail.com>
2014-01-29 01:42:08 +0000

    0

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
 
   end
   def create
-    render 'new'    
+    user=User.find_by(email: params[:session][:email].downcase) 
+    if user&&user.authenticate(params[:session][:password])
+      
+    else
+      flash[:error]='invalid email or password'
+      render 'new'
+    end
   end
   def destroy
     
