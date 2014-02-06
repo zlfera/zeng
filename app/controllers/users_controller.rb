fd545c54f43e57d214f277f9f1e24e3de4f7ef5d
曾令发 <zlfera@gmail.com>
2014-02-06 00:05:32 +0000

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success]='profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user=User.new(user_params)
     if @user.save
