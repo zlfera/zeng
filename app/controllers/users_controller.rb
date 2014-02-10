93681fd0ce0a3b3a6742125074206d3496f6813b
曾令发 <zlfera@gmail.com>
2014-02-10 02:09:09 +0000

    0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :signed_in_user,only: [:edit,:update]
+  before_action :correct_user,only: [:edit,:update]
   def update
     @user=User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -35,4 +36,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 
+  def signed_in_user
+    redirect_to signin_url,notice: 'please sign in' unless signed_in?
+  end
+
+  def correct_user
+    @user=User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
 end
